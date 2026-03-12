61b6bc76257cf0e5b54660f869dc5435906f8b49
vijaysan5 <am.sangavi@gmail.com>
2026-03-12 18:05:46 +0530

    Add files via upload

---
+++ File Handling Base.py
@@ -0,0 +1,40 @@
+"""Sanfolder= open("VS_Base.text",'x')
+print("File is Created")"""
+'''
+abc=open("VS_Base.text", 'a')
+abc.write("\nGood Morning!")
+
+with open("VS_Base.text", 'w') as like:
+    like.write("\nHave a nice day to all...!")
+
+users=open("VS_Base.text", 'a')
+users.write("\nHappy Birthday Dear Friend!")
+print("Created")
+
+with open("VS_Base.text", 'r') as file:
+    look = file.readline()
+    print(look)
+    print(file.readline())
+'''
+""" # Create San.Bio Folder
+San_1=open("San.Bio", 'x')
+print("Bio Page is created")
+"""
+""" #San BIO DATA
+with open("San.Bio",'w') as bio:
+    bio.write("BIO DATA")
+    bio.write("\n Name   : SANGAVI A")
+    bio.write("\n Mobile : 86xxxxxx27")
+    bio.write("\n Degree : M.A., D.SS.,")
+    bio.write("\n Mail ID: am.xxx@gmail.com")
+    print("San Bio is Created")
+"""
+"""Detailz
+Detailz=open ("San.Bio", 'w')
+Detailz.write("  NAME       PHONE     QUALIFICATION  JOINING")
+Detailz.write("\nSandhiya   91xxxxxx98    M.Sc.,       03/2021")
+Detailz.write("\nDhanvi     98xxxxxx09    M.Com.,      12/2023")
+Detailz.write("\nHarini     74xxxxxx87    HSC          12/2022")
+Detailz.write("\nJenifer    93xxxxxx23    MBA          05/2025")
+"""
+
